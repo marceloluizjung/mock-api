a58b6fa38d7c65babaf31b86c328372ab9e5ca81
Marcelo Jung <marcelo_4940@hotmail.com>
2025-08-31 11:26:14 -0300

    Ajustes de configs

--- mock-api/mockapi.js
+++ mock-api/mockapi.js
@@ -2,9 +2,13 @@ const https = require("https");
 const bodyParser = require('body-parser');
 const fs = require("fs");
 const express = require("express");
+const cors = require('cors')
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 https
   .createServer({
       key: fs.readFileSync("key.pem"),
@@ -23,10 +27,7 @@ app.get('/*', (req, res) => {
   res.json({message: 'Hello World, from express'});
 });
 app.put('/*', (req, res) => {
-  console.log(req.body.employeeCompetencyAppraisalIdList);
-  var response = [];
-  req.body.employeeCompetencyAppraisalIdList.forEach(item => { response.push({"id": item,"jobPositionName": "Job Atualizado" + item,"workstationName": "Works Atualizado" + item}); });
-  res.json(response);
+  res.json({message: 'Hello World, from express'});
 });
 app.post('/*', (req, res) => {
   res.json({message: 'Hello World, from express'});
